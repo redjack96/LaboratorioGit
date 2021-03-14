453af9d7dbfe87fa2a992bd853269d70920b8416
Giacomo <giacomo.redjack@gmail.com>
2021-03-14 13:57:56 +0100

    Removed unnecessary variable

--- LaboratorioGit/src/com/giacomo/rossi/HelloGitLog.java
+++ LaboratorioGit/src/com/giacomo/rossi/HelloGitLog.java
@@ -3,9 +3,8 @@ package com.giacomo.rossi;
 public class HelloGitLog {
 
 	public static void main(String[] args) {
-		int i = 0;
 		System.out.println("Hello Git");
-		System.out.println(i+4);
+		System.out.println(4);
 	}
 
 }
