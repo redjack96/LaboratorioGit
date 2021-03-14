6cb61fd2fb1bd5631db7c5000a8c9f79c6bfc0b4
Giacomo <giacomo.redjack@gmail.com>
2021-03-14 19:06:46 +0100

    added code for parse commit id of commits with a specific word in it

--- LaboratorioGit/src/com/giacomo/rossi/HelloGitLog.java
+++ LaboratorioGit/src/com/giacomo/rossi/HelloGitLog.java
@@ -3,8 +3,7 @@ package com.giacomo.rossi;
 public class HelloGitLog {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git");
-		System.out.println(4);
+		ParseGitLog.parseCommitID("added");
 	}
 
 }
--- LaboratorioGit/src/com/giacomo/rossi/ParseGitLog.java
+++ LaboratorioGit/src/com/giacomo/rossi/ParseGitLog.java
@@ -1,14 +1,46 @@
 package com.giacomo.rossi;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
 public class ParseGitLog {
-	
-	private String getLogString(String grep){
+
+	private static String executeGitLogCommand(String grep) {
 		// esegui il comando git log --grep=grep
-		return "no string";
+		String lineOutput = null;
+		StringBuilder bOutput = new StringBuilder();
+		System.out.println("Messaggi di commit che contengono la parola " + grep);
+		try {
+			// Execute command
+			Runtime rt = Runtime.getRuntime();
+			String[] commands = { "git", "log", "--grep=" + grep, "--pretty=oneline" };
+			Process proc = rt.exec(commands);
+
+			BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+
+			BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+
+			// Read the output from the command
+			while ((lineOutput = stdInput.readLine()) != null) {
+				bOutput.append(lineOutput).append('\n');
+			}
+
+			// Read any errors from the attempted command
+			while ((lineOutput = stdError.readLine()) != null) {
+				System.out.println(lineOutput);
+			}
+		} catch (IOException io) {
+			io.printStackTrace();
+		}
+		return bOutput.toString();
 	}
-	
-	public void parseCommitID() {
-		String log = getLogString("added");
-		System.out.println("5ab6b2f");
+
+	public static void parseCommitID(String grep) {
+		String log = executeGitLogCommand(grep);
+		for (String linea : log.lines().collect(Collectors.toList())) {
+			System.out.println(linea.split(" ")[0]);
+		}
 	}
 }
