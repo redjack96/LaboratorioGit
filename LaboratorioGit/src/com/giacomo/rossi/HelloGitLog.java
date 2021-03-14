6b6b6f2847aacec71aebf1539a26d3936210c324
Giacomo <giacomo.redjack@gmail.com>
2021-03-14 13:56:29 +0100

    added a simple sum

--- LaboratorioGit/src/com/giacomo/rossi/HelloGitLog.java
+++ LaboratorioGit/src/com/giacomo/rossi/HelloGitLog.java
@@ -3,7 +3,9 @@ package com.giacomo.rossi;
 public class HelloGitLog {
 
 	public static void main(String[] args) {
+		int i = 0;
 		System.out.println("Hello Git");
+		System.out.println(i+4);
 	}
 
 }
